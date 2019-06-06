38483fd57d4066e7dff7f0988228af59dadf204b
dalon1 <dannelvision@gmail.com>
2019-06-05 23:03:35 -0400

    adding python apis

--- src/app.py
+++ src/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask import jsonify
+from model import Model
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/welcome')
 def welcome():
-    print("Welcome, dannel")
+    return "Welcome, dannel"
 
-if __name__ == 'main':
+@app.route('/welcome/<name>')
+def welcome_name(name):
+    return "Welcome, " + name
+
+@app.route('/user')
+def get_user():
+    user = Model
+    return jsonify(user.get_user())
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8082')
